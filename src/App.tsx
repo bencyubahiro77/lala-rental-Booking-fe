af6497289e2193f426bc700fea3e25af26e8997e
Benjamin Cyubahiro <benjamin.cyubahiro@amalitech.com>
2025-02-16 17:08:53 +0200

    saved data from google response instead of using token

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import axios from 'axios';
-import { jwtDecode } from "jwt-decode";
 
 function App() {
   useEffect(() => {
@@ -8,31 +7,23 @@ function App() {
     const hash = window.location.hash;
     if (hash.includes('access_token')) {
       const token = new URLSearchParams(hash.substring(1)).get('access_token') as any;
-      console.log('Token:', token);
 
       // Send the token to the backend using axios
       axios.post('http://localhost:5000/auth/callback', {
         accessToken: token
       })
       .then(response => {
-        console.log('Backend Response:', response.data);
-
-        // Save the token in localStorage
-        localStorage.setItem('accessToken', token);
-        const decodedToken = jwtDecode(token) as any;
          // Extract and store the details in localStorage
-         const userEmail = decodedToken.email;
-         const fullName = decodedToken.user_metadata.full_name || 'Unknown';
-         const firstName = fullName.split(' ')[0];
-         const lastName = fullName.split(' ')[1] || '';
+         const email = response.data.user.email;
+         const firstName = response.data.user.firstName
+         const lastName = response.data.user.lastName
+         const role = response.data.user.role
  
          localStorage.setItem('accessToken', token);
-         localStorage.setItem('userEmail', userEmail);
+         localStorage.setItem('email', email);
          localStorage.setItem('firstName', firstName);
          localStorage.setItem('lastName', lastName);
-
-        // Clear the URL and redirect to the home page
-        window.location.href = "/";
+         localStorage.setItem('role', role);
       })
       .catch(error => {
         console.error('Error sending token to backend:', error);
